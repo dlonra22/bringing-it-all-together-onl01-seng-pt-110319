4c8d0d3a77c9adf3765a8092d9129e03dfc24e53
arnie b <arnold.bwaila@gmail.com>
2020-03-15 21:59:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -57,8 +57,8 @@ class Dog
   end
 
   def self.new_from_db(row)
-      id = row[0],
-      name = row[1],
+      id = row[0]
+      name = row[1]
       breed = row[2]
     self.new(id,name,breed)
   end
